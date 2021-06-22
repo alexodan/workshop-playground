5e80648bc6310a5a861c2e09cb0b3334613d0010
Alexis Diaz <alexdan1993@gmail.com>
2021-06-21 21:41:23 -0300

    First commit.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CareerPath from "./components/CareerPath";
+
+function App() {
+  return (
+    <div>
+      <CareerPath topics={[]} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CareerPath.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type Topic = {
+    description: string;
+}
+
+type CareerPathProps = {
+    topics: Topic[];
+}
+
+const CareerPath = ({ topics }: CareerPathProps) => {
+    return (
+        <div>
+            <ul>
+                { topics.map((topic: Topic) => <li>{topic.description}</li>) }
+            </ul>
+        </div>
+    );
+}
+
+export default CareerPath;
